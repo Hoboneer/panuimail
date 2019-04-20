8d9ad9f1ba46334531f12014807948f488819eb4
Hoboneer <gabriel.lisaca@gmail.com>
2019-04-20 12:52:58 +1200

    Implement checks before script is run
    Perhaps `blockondates.py` should ignore invalid values? (and just log
    that it ignored them?).

---
+++ sendpanui.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+PANUI_LOG_FILE=panui.log
+
+# Prevent script from running during holidays
+if ! $(awk 'NF == 1 { print $1, $1 } NF == 2 { print $1, $2 }' ${HOLIDAYS:='holidays.txt'} | python blockondates.py 2>> $PANUI_LOG_FILE);
+then
+	exit 1
+fi
+
+
