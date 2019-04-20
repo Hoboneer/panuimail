19b5bd9eaabbadadcfd3c2cf03df2d452d3fbafb
Hoboneer <gabriel.lisaca@gmail.com>
2019-04-20 13:08:14 +1200

    Implement script to prevent running on weekends
    Perhaps `failonweekend.py` should be silent on exiting? (no printing)

---
+++ failonweekend.py
@@ -0,0 +1,10 @@
+import sys
+from datetime import date
+
+if __name__ == "__main__":
+    today = date.today()
+    # Monday is 0.
+    if today.weekday() > 4:
+        print("Failing because it is a weekend", file=sys.stderr)
+        sys.exit(1)
+    # Exit code 0.
