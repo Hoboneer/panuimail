4fabd9ff669c698f46838c29497689e6844296d0
Hoboneer <gabriel.lisaca@gmail.com>
2019-04-26 03:10:07 +1200

    Remove trailing NUL from notices

--- cleannotices.sh
+++ cleannotices.sh
@@ -3,4 +3,6 @@
 # `tr` is called a bunch of times to make this script portable.
 # That is, setting `RS="\0"` (NUL) in `awk` is not portable between
 # implementations. Therefore, chars must be translated.
-cat - | expand -t 1 | tr '\n' '\t' | tr '\0' '\n'
+#
+# Remove trailing null character. This caused an extra notice to appear.
+cat - | tr -s '\0' | tr '\n' '\t' | tr '\0' '\n'
