01f6a3abc0b2619b1e224da14ccc2e7311b52d07
Hoboneer <gabriel.lisaca@gmail.com>
2019-04-26 18:05:52 +1200

    Fix formatting

--- noticedata.sh
+++ noticedata.sh
@@ -12,7 +12,7 @@ echo "$1" | hxselect -ic -s '\t' '.vcex-blog-entry-title > a::attr(href)'
 # Print entry date.
 echo "$1" | hxselect -ic -s '\t' '.vcex-blog-entry-date'
 # Print entry excerpt. Removes any newlines while preserving word separation.
-
+#
 # Excerpts do not require a tab after since it is the last field before
 # the newline.
 echo "$1" | hxselect -ic '.vcex-blog-entry-excerpt' | tr '\n' ' '
