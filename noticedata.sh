f3669a420ca1516d956f848b1fe9295e38dea415
Hoboneer <gabriel.lisaca@gmail.com>
2019-04-26 14:54:36 +1200

    noticedata.sh: Separate each field by TAB (\t) char
    This ensures that each notice is on one line, thereby making the NUL
    char separator (\0) unnecessary. It can just be used straight by any AWK
    implementation.

--- noticedata.sh
+++ noticedata.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
 # This is a program to print out the data necessary to reconstruct a panui
-# notice.
-
+# notice. This prints out the notice in 4 columns separated by tab chars.
+#
 # "$1" refers to the XHTML to filter.
 
 # Print title of entry.
-echo $1 | echo `hxselect -ic '.vcex-blog-entry-title > a::attr(title)'`
+echo "$1" | hxselect -ic -s '\t' '.vcex-blog-entry-title > a::attr(title)'
 # Print link to entry.
-echo $1 | echo `hxselect -ic '.vcex-blog-entry-title > a::attr(href)'`
+echo "$1" | hxselect -ic -s '\t' '.vcex-blog-entry-title > a::attr(href)'
 # Print entry date.
-echo $1 | echo `hxselect -ic '.vcex-blog-entry-date'`
-# Print entry excerpt.
-echo $1 | echo `hxselect -ic '.vcex-blog-entry-excerpt'`
+echo "$1" | hxselect -ic -s '\t' '.vcex-blog-entry-date'
+# Print entry excerpt. Removes any newlines while preserving word separation.
 
-# Allow notices to be separated by null chars (used by xargs).
-printf "\0"
+# Excerpts do not require a tab after since it is the last field before
+# the newline.
+echo "$1" | hxselect -ic '.vcex-blog-entry-excerpt' | tr '\n' ' '
 
+printf "\n"
